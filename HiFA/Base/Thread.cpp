21f1d5e891fa26bd68f5fe3cb512836ce2e71425
as981242002 <875229565@qq.com>
2019-03-19 06:56:44 -0700

    update thread

--- HiFA/Base/Thread.cpp
+++ HiFA/Base/Thread.cpp
@@ -1,6 +1,27 @@
+#include<iostream>
+#include<memory>
+#include<stdint.h>
+#include<errno.h>
+#include<assert.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/prctl.h>
+#include<sys/types.h>
+#include<linux/unistd.h>
 #include "Thread.hpp"
+#include "CountDownLatch.hpp"
 
-Thread::Thread()
+using namespace std;
+
+namespace CurrentThread
 {
+__thread int t_cachedTid = 0;
+__thread char t_tidString[32];
+__thread int t_tidStringLength = 6;
+__thread const char* t_threadName = "DefaultName";
+}
 
+pid_t gettid()
+{
+    return static_cast<pid_t>(::syscall(SYS_gettid));
 }
--- HiFA/Base/Thread.hpp
+++ HiFA/Base/Thread.hpp
@@ -7,12 +7,40 @@
 #include<unistd.h>
 #include<pthread.h>
 #include<sys/syscall.h>
+#include "CountDownLatch.hpp"
+#include "NonCopyable.h"
 
-
-class Thread
+class Thread: NonCopyable
 {
 public:
-    Thread();
+    using ThreadFunc = std::function<void()>;
+    explicit Thread(const ThreadFunc&, const std::string& name = std::string());
+    ~Thread();
+    void start();
+    int join();
+    bool started() const
+    {
+        return started_;
+    }
+
+    pid_t tid()
+    {
+        return tid_;
+    }
+
+    const std::string name() const
+    {
+        return name_;
+    }
+private:
+    void setDefaultName();
+    bool started_;
+    bool joined_;
+    pthread_t pthreadId_;
+    pid_t tid_;
+    ThreadFunc func_;
+    std::string name_;
+    CountDownLatch latch_;
 };
 
 #endif // THREAD_HPP
