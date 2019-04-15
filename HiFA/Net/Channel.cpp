2bf0694852ba76802953f303f0b079d1eb501be8
as981242002 <875229565@qq.com>
2019-04-14 20:21:57 -0700

    add Channel

---
+++ HiFA/Net/Channel.cpp
@@ -0,0 +1,5 @@
+#include<unistd.h>
+#include<queue>
+#include<cstdlib>
+#include<iostream>
+#include"Channel.hpp"
---
+++ HiFA/Net/Channel.hpp
@@ -0,0 +1,133 @@
+#ifndef NET_CHANNEL_HPP
+#define NET_CHANNEL_HPP
+
+#include <string>
+#include <unordered_map>
+#include <memory>
+#include <sys/epoll.h>
+#include <functional>
+#include "Timer.hpp"
+
+class EventLoop;
+class HttpData;
+
+class Channel
+{
+private:
+    using CallBack = std::function<void()>;
+    EventLoop* loop_;
+    int fd_;
+    uint32_t events_;
+    uint32_t revents_;
+    uint32_t lastevents_;
+
+    std::weak_ptr<HttpData> holder_;
+private:
+    int parse_URI();
+    int parse_Header();
+    int analysisRequest();
+
+    CallBack readHandler_;
+    CallBack writeHandler_;
+    CallBack errorHandler_;
+    CallBack connHandler_;
+
+public:
+    Channel(EventLoop* loop);
+    Channel(EventLoop* loop, int fd);
+    ~Channel();
+    int getFd();
+    void setFd();
+
+    void setHolder(std::shared_ptr<HttpData> holder)
+    {
+        holder_ = holder;
+    }
+
+    std::shared_ptr<HttpData> getHolder()
+    {
+        return  holder_.lock();
+    }
+
+    void setReadHandler(CallBack&& readHandler)
+    {
+        readHandler_ = readHandler;
+    }
+
+    void setWriteHandler(CallBack&& writeHandler)
+    {
+        writeHandler_ = writeHandler;
+    }
+
+    void setErrorHandler(CallBack&& errorHandler)
+    {
+        errorHandler_ = errorHandler;
+    }
+
+    void setConnHandler(CallBack&& connHandler)
+    {
+        connHandler = connHandler_;
+    }
+
+    void handleEvents()
+    {
+        events_ = 0;
+        if((revents_ & EPOLLHUP) && !(revents_ & EPOLLIN))
+        {
+            events_ = 0;
+            return;
+        }
+        if(revents_ & EPOLLERR)
+        {
+            if(errorHandler_)
+                errorHandler_();
+            events_ = 0;
+            return;
+        }
+        if(revents_ & (EPOLLIN | EPOLLPRI | EPOLLHUP))
+        {
+            handleRead();
+        }
+
+        if(revents_ & EPOLLOUT)
+        {
+            handleWrite();
+        }
+
+    }
+
+    void handleRead();
+    void handleWrite();
+    void handleError(int fd, int err_num, std::string short_msg);
+    void handleConn();
+
+    void setRevents(uint32_t ev)
+    {
+        revents_ = ev;
+    }
+
+    void setEvents(uint32_t ev)
+    {
+        events_ = ev;
+    }
+
+    uint32_t& getEvents()
+    {
+        return events_;
+    }
+
+    bool EqualAndUpdateLastEvents()
+    {
+        bool ret = (lastevents_ == events_);
+        lastevents_ = events_;
+        return ret;
+    }
+
+    uint32_t getLastEvents()
+    {
+        return lastevents_ ;
+    }
+};
+
+using SP_Channel = std::shared_ptr<Channel>;
+#endif // CHANNEL_HPP
